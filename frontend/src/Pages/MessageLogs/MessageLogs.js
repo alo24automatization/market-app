18c4563d0e222682bc857b86ebda12090f8bfbae
Abdurakhmonov Sharif <sharifabduraxmonov@mail.ru>
2024-10-20 23:12:05 +0500

    update messageLogger deleted then and catch

--- frontend/src/Pages/MessageLogs/MessageLogs.js
+++ frontend/src/Pages/MessageLogs/MessageLogs.js
@@ -1,69 +1,35 @@
-import React, { useState } from 'react';
-import Api, { baseURL } from '../../Config/Api.js';
+import React, {useState} from 'react'
+import Api, {baseURL} from '../../Config/Api.js'
 
 const MessageLogs = () => {
-    const [isSending, setIsSending] = useState(false);
-    const [logContainer, setLogContainer] = useState([]);
+    const [isSending, setIsSending] = useState(false)
+    const [logContainer, setLogContainer] = useState([])
 
-    const startSendingMessages = () => {
-        if (isSending) return;
+    const startSendingMessages = async () => {
+        if (isSending) return
 
-        setIsSending(true);
-
+        setIsSending(true)
+        try {
+            await Api.post('/send_message_dev')
+        } catch (error) {
+            console.error('Error:', error)
+            setIsSending(false)
+        }
         // Use fetch to stream the logs
-        Api.post('/send_message_dev')
-            .then((response) => {
-                const reader = response.body.getReader();
-                const decoder = new TextDecoder();
-
-                const readStream = () => {
-                    reader.read().then(({ done, value }) => {
-                        if (done) {
-                            console.log("Stream finished");
-                            setIsSending(false);
-                            return;
-                        }
-
-                        const chunk = decoder.decode(value);
-                        const logs = chunk.split("\n").filter(Boolean);  // Split by newlines
-
-                        logs.forEach((logStr) => {
-                            const data = JSON.parse(logStr);  // Parse each JSON chunk
-                            setLogContainer((prevLogs) => [
-                                ...prevLogs,
-                                { log: data.log, isError: data.isError },
-                            ]);
-
-                            if (data.log === "Task completed") {
-                                alert("All messages sent!");
-                            }
-                        });
-
-                        // Continue reading next chunk
-                        readStream();
-                    });
-                };
-
-                readStream();
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-                setIsSending(false);
-            });
-    };
+     }
 
     const stopSendingMessages = () => {
-        if (!isSending) return;
+        if (!isSending) return
 
         Api.post('/stop_message_sending')
-            .then(({ data }) => {
-                alert(data.message);
-                setIsSending(false);
+            .then(({data}) => {
+                alert(data.message)
+                setIsSending(false)
             })
             .catch((error) => {
-                console.error('Error stopping messages:', error);
-            });
-    };
+                console.error('Error stopping messages:', error)
+            })
+    }
 
     return (
         <div className='py-2 pt-16 md:pt-2 bg-black-800 px-1 h-full overflow-y-auto'>
@@ -85,7 +51,10 @@ const MessageLogs = () => {
             </div>
             <div id='log-container' className='overflow-y-auto p-2'>
                 {logContainer.map((log, index) => (
-                    <p key={index} className={log.isError ? 'error-log' : 'success-log'}>
+                    <p
+                        key={index}
+                        className={log.isError ? 'error-log' : 'success-log'}
+                    >
                         {log.isError
                             ? `Error: ${log.log} ❌`
                             : `Success: ${log.log} ✅`}
@@ -93,7 +62,7 @@ const MessageLogs = () => {
                 ))}
             </div>
         </div>
-    );
-};
+    )
+}
 
-export default MessageLogs;
+export default MessageLogs
